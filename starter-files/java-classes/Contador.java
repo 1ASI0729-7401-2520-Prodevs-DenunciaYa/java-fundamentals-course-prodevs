1c3c0628469772e0a4cf9523c8a57f26b19d6904
Gabriel Braithuaite <u20201e889@upc.edu.pe>
2025-11-16 14:14:04 -0500

    Add Contador class to print even numbers from 10 to 20

---
+++ starter-files/java-classes/Contador.java
@@ -0,0 +1,8 @@
+public class Contador {
+    public static void main(String[] args) {
+        for (int i = 10; i <= 20; i+=2) {
+            System.out.println("Número: " + i);
+        }
+    }
+}
+
